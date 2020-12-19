733397493a47a1ba4e70c0c6a457307acfaf4801
umairk80 <71974442+umairk80@users.noreply.github.com>
2020-12-19 17:51:27 +0000

    add spring io stuff

---
+++ src/test/java/com/umair/pipeline/PipelineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.umair.pipeline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PipelineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
